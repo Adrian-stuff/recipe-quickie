c544ac10c7597dccdf50a256ebdba74be34b57ab
Adrian De Vera <mahalkosariliko6@gmail.com>
2025-05-01 22:25:46 +0800

    init

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -11,8 +11,7 @@
 			return;
 		}
 		console.log('Creating recipe with ingredients:', ingredients);
-		// Add your logic to create a recipe here
-		// curl -X POST -H "Content-Type: application/json" -d '{"ingredients": ["chicken", "rice", "soy sauce", "ginger", "garlic"]}' http://127.0.0.1:8080/recipe
+
 		await fetch('http://127.0.0.1:8080/generate_recipe', {
 			method: 'POST',
 			headers: {
@@ -35,7 +34,7 @@
 
 <div class="mt-8">
 	{#each ingredients as val, i}
-		<div class="my-2 px-15">
+		<div class="px-15 my-2">
 			<div class="flex flex-row items-center justify-between rounded-lg bg-[#B58C72] px-4 py-2">
 				<div class="">{val}</div>
 				<Button
@@ -54,7 +53,7 @@
 	{#if ingredients.length == 0}
 		<div class="mb-8 text-center">Add Ingredients</div>
 	{/if}
-	<div class="my-2 flex flex-row items-center justify-center px-15">
+	<div class="px-15 my-2 flex flex-row items-center justify-center">
 		<TextField name="Enter Ingredient" bind:value={newVal} />
 		<Button
 			type="text"
@@ -71,9 +70,9 @@
 		</Button>
 	</div>
 
-	<div class="absolute right-7 bottom-10 z-10">
+	<div class="absolute bottom-10 right-7 z-10">
 		<button
-			class="flex h-7 w-13 items-center justify-center rounded-lg bg-blue-500 p-4 font-bold text-white shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+			class="w-13 flex h-7 items-center justify-center rounded-lg bg-blue-500 p-4 font-bold text-white shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
 			onclick={createRecipe}
 			aria-label="Add Item"
 		>
